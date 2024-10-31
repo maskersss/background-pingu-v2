2605ccaf7b24421815dff93686aa5fe0f0e7c0e7
maskers <97827489+mskrss@users.noreply.github.com>
2024-10-31 13:17:07 +0300

    bump old prism version to 8.x

--- BackgroundPingu/core/issues.py
+++ BackgroundPingu/core/issues.py
@@ -1050,7 +1050,7 @@ class IssueChecker:
         ):
             builder.error("old_mod_version", "PeepoPractice", "https://github.com/faluhub/peepoPractice/releases/latest/")
 
-        if self.log.has_pattern(r"^Prism Launcher version: [2-7]"):
+        if self.log.has_pattern(r"^Prism Launcher version: [2-8]"):
             if self.log.has_pattern(r"^Prism Launcher version: [8-8]"):
                 builder.note("semi_old_prism_version")
             else:
