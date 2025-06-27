cee8084079a4476f40da8a4302ffd63374c14d05
maskers <97827489+mskrss@users.noreply.github.com>
2025-06-27 13:49:58 +0300

    reduce max log lines

--- BackgroundPingu/config.py
+++ BackgroundPingu/config.py
@@ -1,4 +1,4 @@
 LINK_PATTERN = r"https:\/\/(?:api\.)?paste\.ee\/.\/\w+|https:\/\/mclo\.gs\/\w+|https?:\/\/[\w\-_\/.]+\.(?:txt|log|tdump)\?ex=[^&]+&is=[^&]+&hm=[^&]+&|https?:\/\/[\w\-_\/.]+\.(?:txt|log|tdump)"
 
-MAX_STARTING_LOG_LINES = 6000
-MAX_ENDING_LOG_LINES = 10000
+MAX_STARTING_LOG_LINES = 3500
+MAX_ENDING_LOG_LINES = 6000
