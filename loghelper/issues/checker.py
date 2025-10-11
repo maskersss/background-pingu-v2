2f955dbb9ec2483f5d31c58d01a35e073f1b2857
maskers <97827489+mskrss@users.noreply.github.com>
2025-10-11 21:26:34 +0300

    bump ranked version

--- loghelper/issues/checker.py
+++ loghelper/issues/checker.py
@@ -1034,7 +1034,7 @@ class IssueChecker:
             extracted_version = match.group(1)
             try:
                 extracted_version = version.parse(extracted_version)
-                needed_version = version.parse("5.1.12")
+                needed_version = version.parse("5.4.0")
 
                 if extracted_version < needed_version:
                     builder.error("old_mod_version", "MCSR Ranked", "https://modrinth.com/mod/mcsr-ranked/versions/")
