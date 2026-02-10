da081692003976e2f641a7b6d0e77b248d5263a0
maskers <97827489+mskrss@users.noreply.github.com>
2026-02-10 19:39:23 +0300

    update mclo.gs regex

--- loghelper/parser.py
+++ loghelper/parser.py
@@ -77,7 +77,7 @@ class Log:
     @staticmethod
     def from_link(link: str, timeout: int = 5):
         paste_ee_match = re.search(r"https://(?:api\.)?(?:pastee\.dev|paste\.ee)/(?:p/|d/)([a-zA-Z0-9]+)", link)
-        mclogs_match = re.search(r"https://mclo\.gs/(\w+)", link)
+        mclogs_match = re.search(r"https://(?:api\.)?mclo\.gs/(?:1/raw/)?(\w+)", link)
         if paste_ee_match: link = f"https://pastee.dev/d/{paste_ee_match.group(1)}/0"
         elif mclogs_match: link = f"https://api.mclo.gs/1/raw/{mclogs_match.group(1)}"
         elif not ".txt" in link and not ".log" in link and not ".tdump" in link and not ".json" in link: return None
