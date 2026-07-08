af0450049fdaae1844e076bbf72478b103d7137b
maskers <97827489+mskrss@users.noreply.github.com>
2026-07-08 09:34:31 +0300

    bug fix

--- loghelper/parser.py
+++ loghelper/parser.py
@@ -789,7 +789,8 @@ class Log:
             match = re.search(crash_pattern, log, re.DOTALL)
             if not match is None:
                 stacktrace = match.group().lower()
-                stacktrace = stacktrace.replace(self.minecraft_folder.lower(), "")
+                if not self.minecraft_folder is None:
+                    stacktrace = stacktrace.replace(self.minecraft_folder.lower(), "")
                 return i, stacktrace
         
         return None, None
